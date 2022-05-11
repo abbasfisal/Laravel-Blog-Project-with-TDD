caf60722f167674c61da522bf146074385ff6e67
Abbas Momeni <abbasmomeny1994@gmail.com>
2022-05-11 08:46:33 +0430

    [create] migrations / models - [edit] some configs

---
+++ database/migrations/2022_05_11_034740_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+
+            $table->foreignId('writer_id')
+                ->constrained('users')->references('id')
+                ->cascadeOnUpdate();
+
+            $table->string('cover');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ database/migrations/2022_05_11_034800_create_comments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+
+
+            $table->foreignId('user_id')
+                ->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->foreignId('post_id')
+                ->constrained('posts')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->foreignId('reply_id')
+                ->constrained('comments')->references('id')
+                ->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->boolean('show');
+
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
